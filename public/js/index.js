1319f4aca762761e9f321a7f685c757d176591b0
Manuela <fmanuela499@gmail.com>
2018-02-15 21:48:28 -0500

    funcionalidad y extrayendo datos a local storage

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,26 @@
 'use strict';
 
-var testing = alert(':D');
+// Setup an event listener to make an API call once auth is complete
+function onLinkedInLoad() {
+  IN.Event.on(IN, 'auth', getResult);
+};
+
+var onSuccess = function onSuccess(data) {
+  localStorage.firstName = data.firstName;
+  localStorage.lastName = data.lastName;
+  localStorage.headline = data.lastName;
+  localStorage.siteStandardProfileRequest = data.siteStandardProfileRequest.url;
+};
+
+var getData = function getData(data) {
+  localStorage.numConnections = data.numConnections;
+};
+
+var onError = function onError(error) {
+  console.log(error);
+};
+
+var getResult = function getResult() {
+  IN.API.Raw('/people/~').result(onSuccess).error(onError);
+  IN.API.Raw('people/~:(id,num-connections,picture-url)').result(getData).error(onError);
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-let testing = alert(':D');
+// Setup an event listener to make an API call once auth is complete
+function onLinkedInLoad() {
+  IN.Event.on(IN, 'auth', getResult);
+};
+
+let onSuccess = (data) => {
+  localStorage.firstName = data.firstName;
+  localStorage.lastName = data.lastName;
+  localStorage.headline = data.lastName;
+  localStorage.siteStandardProfileRequest = data.siteStandardProfileRequest.url;
+};
+
+let getData = (data) => {
+  localStorage.numConnections = data.numConnections;
+};
+
+let onError = (error) => {
+  console.log(error);
+};
+
+let getResult = () => {
+  IN.API.Raw('/people/~').result(onSuccess).error(onError);
+  IN.API.Raw('people/~:(id,num-connections,picture-url)').result(getData).error(onError);
+};
