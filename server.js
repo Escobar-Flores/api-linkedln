2ea2839ad6e4b29715d2b246017d33bacf030d6e
Manuela <fmanuela499@gmail.com>
2018-02-15 20:15:41 -0500

    instalando servidor

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+var server = app.listen(3000, listening);
+
+function listening() {
+  console.log('Listening :D');
+}
+
+app.use(express.static('public'));
